a22991b48e7cdf4a7b41de7c798c55727828cf3e
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2018-05-02 22:38:28 +0300

    handle the store

--- todomvc.go
+++ todomvc.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"todomvc/store"
+	"todomvc/store/model"
+
 	"github.com/gopherjs/vecty"
 	"github.com/gopherjs/vecty/elem"
 )
 
 type myComponent struct {
 	vecty.Core
+	Items        []*model.Item `vecty:"prop"`
+	newItemTitle string
 }
 
 func (mc *myComponent) Render() vecty.ComponentOrHTML {
 	return elem.Body(
 		elem.Div(
 			vecty.Markup(vecty.Class("my-main-container")),
-			vecty.Text("Welcome to my site"),
+			vecty.Text("Hadi LAN!"),
 		),
 	)
 }
 
 func main() {
+	vecty.SetTitle("GopherJS • TodoMVC")
+	vecty.AddStylesheet("todomvc/styles/base.css")
+	vecty.AddStylesheet("todomvc/styles/index.css")
+
 	comp := &myComponent{}
+	store.Listeners.Add(comp, func() {})
+
 	vecty.RenderBody(comp)
 }
